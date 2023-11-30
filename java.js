ce8b479e379e64c9945306a2bf1defe2e15e6441
Philly-P <panzarpa@clarkson.edu>
2023-11-30 12:18:10 -0500

    implemented rest of logic
    the rest of the logic besides a reset at the current moment

--- java.js
+++ java.js
@@ -24,11 +24,54 @@ function play(choice)
     }else{
         tie = tie;
     };
-    //find src img before writing this text
+    //find src img before writing this text 1 = rock; 2 = paper; 3 = scissors
+    setTimeout(() => {
     if(computerChoice === 1 && playerChoice ==="rock"){
         document.getElementById('o-img').src ="";
         document.getElementById('result').innerHTML = "Rock ties with rock. No winner";
+        ++tie;
+    }else if(computerChoice === 1 && playerChoice ==="scissors"){
+        document.getElementById('o-img').src ="";
+        document.getElementById('result').innerHTML = "Scissors losses to rock. You lost";
+        ++loss;
+    }else if(computerChoice === 1 && playerChoice ==="paper"){
+        document.getElementById('o-img').src ="";
+        document.getElementById('result').innerHTML = "Paper beats rock. you win";
+        ++wins;
+    }else if(computerChoice === 2 && playerChoice ==="rock"){
+        document.getElementById('o-img').src ="";
+        document.getElementById('result').innerHTML = "Rock loses to paper. you lose";
+        ++loss;
+    }else if(computerChoice === 2 && playerChoice ==="paper"){
+        document.getElementById('o-img').src ="";
+        document.getElementById('result').innerHTML = "Paper ties with paper. No winner";
+        ++ties;
+    }else if(computerChoice === 2 && playerChoice ==="scissors"){
+        document.getElementById('o-img').src ="";
+        document.getElementById('result').innerHTML = "Scissors beats paper. you win";
+        ++wins;
+    }else if(computerChoice === 3 && playerChoice ==="rock"){
+        document.getElementById('o-img').src ="";
+        document.getElementById('result').innerHTML = "Rock beats Scissors. you win";
+        ++wins;
+    }else if(computerChoice === 3 && playerChoice ==="paper"){
+        document.getElementById('o-img').src ="";
+        document.getElementById('result').innerHTML = "paper loses to Scissors. you lose";
+        ++loss;
+    }else {
+        document.getElementById('o-img').src ="";
+        document.getElementById('result').innerHTML = "Scissors ties with Scissors. No winner";
+        ++ties;
     }
-    
-    
+    sessionStorage("winCount", wins);
+    sessionStorage("lossCount", loss);
+    sessionStorage("tieCount", ties);
+    document.getElementById('wins').innerHTML = sessionStorage.getItem("winCount");
+    document.getElementById('loss').innerHTML = sessionStorage.getItem("lossCount");
+    document.getElementById('ties').innerHTML = sessionStorage.getItem("tieCount");
+    }, 200);
+}
+
+function restart() {
+
 }
