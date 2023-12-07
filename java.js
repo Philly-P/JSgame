d99afb959a8ca8d68ad4e91a56dd151890c07799
Philly-P <panzarpa@clarkson.edu>
2023-12-07 14:13:46 -0500

    working
    now I just need to style it to look better

--- java.js
+++ java.js
@@ -18,7 +18,7 @@ function play(choice)
     }else {
         loss = loss;
     };
-    if(tie = null)
+    if(tie == null)
     {
         tie = 0;
     }else{
@@ -27,51 +27,58 @@ function play(choice)
     //find src img before writing this text 1 = rock; 2 = paper; 3 = scissors
     setTimeout(() => {
     if(computerChoice === 1 && playerChoice ==="rock"){
-        document.getElementById('o-img').src ="";
+        document.getElementById('o-img').src ="svg/icons8-rock-64.png";
         document.getElementById('result').innerHTML = "Rock ties with rock. No winner";
         ++tie;
     }else if(computerChoice === 1 && playerChoice ==="scissors"){
-        document.getElementById('o-img').src ="";
+        document.getElementById('o-img').src ="svg/icons8-rock-64.png";
         document.getElementById('result').innerHTML = "Scissors losses to rock. You lost";
         ++loss;
     }else if(computerChoice === 1 && playerChoice ==="paper"){
-        document.getElementById('o-img').src ="";
+        document.getElementById('o-img').src ="svg/icons8-rock-64.png";
         document.getElementById('result').innerHTML = "Paper beats rock. you win";
         ++wins;
     }else if(computerChoice === 2 && playerChoice ==="rock"){
-        document.getElementById('o-img').src ="";
+        document.getElementById('o-img').src ="svg/icons8-paper-50.png";
         document.getElementById('result').innerHTML = "Rock loses to paper. you lose";
         ++loss;
     }else if(computerChoice === 2 && playerChoice ==="paper"){
-        document.getElementById('o-img').src ="";
+        document.getElementById('o-img').src ="svg/icons8-paper-50.png";
         document.getElementById('result').innerHTML = "Paper ties with paper. No winner";
-        ++ties;
+        ++tie;
     }else if(computerChoice === 2 && playerChoice ==="scissors"){
-        document.getElementById('o-img').src ="";
+        document.getElementById('o-img').src ="svg/icons8-paper-50.png";
         document.getElementById('result').innerHTML = "Scissors beats paper. you win";
         ++wins;
     }else if(computerChoice === 3 && playerChoice ==="rock"){
-        document.getElementById('o-img').src ="";
+        document.getElementById('o-img').src ="svg/icons8-scissors-50.png";
         document.getElementById('result').innerHTML = "Rock beats Scissors. you win";
         ++wins;
     }else if(computerChoice === 3 && playerChoice ==="paper"){
-        document.getElementById('o-img').src ="";
+        document.getElementById('o-img').src ="svg/icons8-scissors-50.png";
         document.getElementById('result').innerHTML = "paper loses to Scissors. you lose";
         ++loss;
-    }else {
-        document.getElementById('o-img').src ="";
+    }else if(computerChoice === 3 && playerChoice ==="scissors"){
+        document.getElementById('o-img').src ="svg/icons8-scissors-50.png";
         document.getElementById('result').innerHTML = "Scissors ties with Scissors. No winner";
-        ++ties;
+        ++tie;
     }
-    sessionStorage("winCount", wins);
-    sessionStorage("lossCount", loss);
-    sessionStorage("tieCount", ties);
+    sessionStorage.setItem("winCount", wins);
+    sessionStorage.setItem("lossCount", loss);
+    sessionStorage.setItem("tieCount", tie);
     document.getElementById('wins').innerHTML = sessionStorage.getItem("winCount");
     document.getElementById('loss').innerHTML = sessionStorage.getItem("lossCount");
-    document.getElementById('ties').innerHTML = sessionStorage.getItem("tieCount");
+    document.getElementById('tie').innerHTML = sessionStorage.getItem("tieCount");
     }, 200);
 }
 
 function restart() {
+    sessionStorage.setItem("winCount", 0);
+  sessionStorage.setItem("tieCount", 0);
+  sessionStorage.setItem("lossCount", 0);
+  document.getElementById('wins').innerHTML = sessionStorage.getItem("winCount");
+  document.getElementById('tie').innerHTML = sessionStorage.getItem("tieCount");
+  document.getElementById('loss').innerHTML = sessionStorage.getItem("lossCount");
+  document.getElementById('result').innerHTML = "Result";
 
 }
